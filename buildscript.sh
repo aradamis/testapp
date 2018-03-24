bdd44f14e53ff738e4942270227b504a366fc697
Andrew Radamis <andrew@radamis.net>
2018-03-24 19:16:17 -0400

    Persistant shared Maven Repo

--- buildscript.sh
+++ buildscript.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-docker run -it --rm --name my-maven-project -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.5.3-jdk-8 mvn clean install
+if [ -z `docker volume list -q -f name=maven-repo` ]
+then
+  docker volume create maven-repo
+fi
+
+docker run -it --rm --name my-maven-project -v maven-repo:/root/.m2 -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.5.3-jdk-8 mvn clean install
 
 docker build --rm -t testapp .
