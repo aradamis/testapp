d089784f23285ed94d552812caac1e9129433811
Andrew Radamis <andrew@radamis.net>
2018-03-24 16:49:49 -0400

    updates

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM openjdk:8
-COPY testapp-1.0-SNAPSHOT.jar /usr/src/
+ADD target/testapp-1.0-SNAPSHOT.jar /usr/src/
 CMD java -jar /usr/src/testapp-1.0-SNAPSHOT.jar
---
+++ buildscript.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+docker run -it --rm --name my-maven-project -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.3-jdk-8 mvn clean install
+
+docker build --rm -t testapp .
