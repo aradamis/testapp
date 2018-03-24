f788d6e3c2b172dbfff57f91261c22bba1740748
Andrew Radamis <andrew@radamis.net>
2018-03-24 19:37:28 -0400

    Update

--- buildscript.sh
+++ buildscript.sh
@@ -4,7 +4,6 @@ if [ -z `docker volume list -q -f name=maven-repo` ]
 then
   docker volume create maven-repo
 fi
-rm -r target/
 
 docker run -it --rm --name my-maven-project -v maven-repo:/root/.m2 -v "$(pwd)":/usr/src/mymaven -w /usr/src/mymaven maven:3.5.3-jdk-8 mvn clean install
 
