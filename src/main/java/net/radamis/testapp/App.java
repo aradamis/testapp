996961dbb1d85132d9bc0444aa47711fdfca23a6
Andrew Radamis <andrew@radamis.net>
2018-03-24 19:17:28 -0400

    New code

--- src/main/java/net/radamis/testapp/App.java
+++ src/main/java/net/radamis/testapp/App.java
@@ -1,11 +1,30 @@
 package net.radamis.testapp;
 
-/**
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
+import java.nio.charset.Charset;
+import java.net.MalformedURLException;
+import java.io.IOException;
+/*
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+    private static void run() {
+      try{
+        URL url = new URL("http://serenity:8080/ip.php");
+
+        String o = IOUtils.toString(url.openStream(), Charset.defaultCharset());
+
+        System.out.println("O: " + o);
+      }catch(MalformedURLException e){
+        System.out.println("MalformedURLException: " + e.getMessage());
+      }catch(IOException e){
+        System.out.println("IOException: " + e.getMessage());
+      }
+    }
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
